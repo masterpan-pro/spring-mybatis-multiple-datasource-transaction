c70956f17ef4d9d64431a6d59f57ae1295dd119a
MasterPan <mwolfer@foxmail.com>
2018-06-26 11:33:00 +0800

    update

---
+++ src/test/java/com/demo/service/ServiceTest.java
@@ -0,0 +1,12 @@
+package com.demo.service;
+
+import com.demo.BaseJunitTest;
+import org.junit.Test;
+
+public class ServiceTest extends BaseJunitTest {
+
+    @Test
+    public void test0() {
+        System.out.println("hello");
+    }
+}
